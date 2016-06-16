8387c411ff655585a5432cf63f142cb94a6179b2
atungare <ajay.tungare@gmail.com>
2016-06-15 20:16:40 -0700

    rock paper scissors

---
+++ rockPaperScissors.hs
@@ -0,0 +1,14 @@
+module RPS where
+
+import Data.List
+
+options = ["rock", "paper", "scissors"]
+
+roundsRecursive :: Int -> [String] -> [[String]]
+roundsRecursive 0 _ = [[]]
+roundsRecursive n opts = [x:xs | x <- opts, xs <- roundsRecursive (n-1) opts]
+
+roundsFolding  :: Int -> [String] -> [[String]]
+roundsFolding n opts = foldr step [[]] [1..n]
+  where step = (\_ acc -> [x:xs | x <- opts, xs <- acc])
+
