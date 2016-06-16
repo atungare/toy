9ccb6446fb798f160035347d53807ed1b5177736
atungare <ajay.tungare@gmail.com>
2016-06-15 22:09:43 -0700

    use sequenceA for rps

--- rockPaperScissors.hs
+++ rockPaperScissors.hs
@@ -8,6 +8,9 @@ roundsRecursive :: Int -> [String] -> [[String]]
 roundsRecursive 0 _ = [[]]
 roundsRecursive n opts = [x:xs | x <- opts, xs <- roundsRecursive (n-1) opts]
 
-roundsFolding  :: Int -> [String] -> [[String]]
+roundsFolding :: Int -> [String] -> [[String]]
 roundsFolding n opts = foldr step [[]] [1..n]
   where step = (\_ acc -> [x:xs | x <- opts, xs <- acc])
+
+roundsSequenceA :: Int -> [String] -> [[String]]
+roundsSequenceA n opts = sequenceA $ take n $ repeat opts
